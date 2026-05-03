3f794b552ef0004beaeaa45fbd7d724afa52d65a
yeajieo <like.crong@gmail.com>
2026-05-03 22:04:01 +0900

    CLI

--- main_ci.py
+++ main_ci.py
@@ -2,33 +2,50 @@
 # CI 전용 실행 파일 - GUI 없이 args로 값 입력받음
 
 import argparse
+from datetime import datetime
 from utils.makeExampleFiles import ExampleFileCreator
+from utils.makeZip import zipFileCreator
 from utils.makeAppLog import setup_logger
-
 logger = setup_logger()
 
 if __name__ == '__main__':
 
-    # ── 입력값 파싱 ──────────────────────────────
+    logger.info("=========== START")
+
+    # Generate sample log files.
+    example = ExampleFileCreator()
+    example.load_paths()
+    example.load_datetime()
+    example.makeSampleFolder()
+    example.create_allFiles()
+
+    # ── Parsing ──────────────────────────────
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_path', required=True)  # 폴더 경로
     parser.add_argument('--date',        required=True)  # 날짜 (YYYY-MM-DD)
     parser.add_argument('--time',        required=True)  # 시간 (HH-MM-SS)
     args = parser.parse_args()
 
-    # ── 날짜/시간 파싱 ───────────────────────────
-    year, month, day       = args.date.split('-')
-    hour, minute, second   = args.time.split('-')
+    targetDate = datetime.strptime(f"{args.date} {args.time}", "%Y-%m-%d %H-%M-%S")
+    logger.debug(f"path : {args.output_path}")
+    logger.debug(f"date : {targetDate}")
 
-    # ── 샘플 파일 생성 ───────────────────────────
-    example = ExampleFileCreator()
-    example.load_paths()
+    mz = zipFileCreator(args.output_path, targetDate)
+    mz.makeWorkspaceFolder()
+    flag = mz.exporting()
+    mz.zipping()
 
-    # GUI 입력값 직접 주입
-    example.paths['output'] = args.output_path
-    example.dt = [year, month, day, hour, minute, second]
+    if flag == True:
+        logger.info(f"All process is successfully done.")
+    else:
+        logger.info(f"No log files.")
+    logger.info("=========== END")
 
-    example.makeSampleFolder()
-    example.create_allFiles()
 
-    logger.info("CI 실행 완료!")
+
+
+
+
+
+
+
